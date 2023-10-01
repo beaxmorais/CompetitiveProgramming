0623559a0e3036f0d0aabebf6e3b56ed55becb60
beaxmorais <beatrizxmoraisc@gmail.com>
2023-10-01 08:10:50 -0300

    fix solution

--- Leetcode/LogestSubstring/3.cpp
+++ Leetcode/LogestSubstring/3.cpp
@@ -1,36 +1,30 @@
+#include <cctype>
 #include <iostream>
 #include <vector>
-#include <cctype>
 using namespace std;
 
-int lengthOfLongestSubstring(string s){
+int lengthOfLongestSubstring(string s) {
 
-    if(s.size() < 1)
-        return 0;
-
-    int max_lenght_substring = 0;
-
-    int begin =0, end =0;
-
-    for(int i =1; i <s.size(); i++){
-        max_lenght_substring = max(max_lenght_substring, (end - begin)+1);
-
-        for(int j=begin; j <=end; j++){
-
-            if (s[j] == s[i]){
-                begin = j + 1;
-            }
-        }
-        end = i;
-    }
-
-    return max_lenght_substring;
-}
-
-int main() {
-    string s;
-    cin>>s;
-
-    cout << lengthOfLongestSubstring(s) <<endl;
+  if (s.size() < 1)
     return 0;
+
+  if (s.size() < 2)
+    return 1;
+
+  int max_lenght_substring = 0;
+
+  int begin = 0, end;
+
+  for (int i = 1; i < s.size(); i++) {
+    end = i;
+    int j = begin;
+
+    while (j < end) {
+      if (s[i] == s[j])
+        begin = j + 1;
+      j++;
+    }
+    max_lenght_substring = max(max_lenght_substring, (end - begin) + 1);
+  }
+  return max_lenght_substring;
 }
