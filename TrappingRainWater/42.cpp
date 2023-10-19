9ab6e615edffa5486b80819221842912f8e75d97
beaxmorais <beatrizxmoraisc@gmail.com>
2023-10-19 11:10:30 -0300

    trapping rain water

--- TrappingRainWater/42.cpp
+++ TrappingRainWater/42.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     int trap(vector<int>& height) {
 
-        int maxIndex = 0;
+        int sum = 0, left = 0, right = 0;
+        vector <int> max_left(height.size(), 0), max_right(height.size(), 0);
 
-        int units = 0, maxIndex = 0;
-        for(int i = 1; i <height.size(); i++){
-            if(height[maxIndex] < height[i])
-                maxIndex = i;
+        for(int i = 0; i <height.size(); i++){
+            left = max(left, height[i]);
+            max_left[i] = left;
         }
 
-        for(int i = maxIndex - 1; i>=0; i--){
-            if(height[i] > height[])
+        for(int i = height.size() -1; i >= 0; i--){
+            right = max(right, height[i]);
+            max_right[i] = right;
         }
+
+
+        for(int i = 0; i<height.size()-1; i++){
+            sum += min(max_left[i], max_right[i]) - height[i];
+        }
+
+        return sum; 
     }
 };
